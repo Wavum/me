9943b40afb07b81c78e4b9a1fda9a09eb6ec5899
Wavum <rl00@gmx.at>
2019-01-30 21:16:55 +0100

    Updates

---
+++ precache-manifest.c58cce0fdfebdf009d301393d5e86281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3595b8abae663447055e",
+    "url": "/static/css/main.9d4ea480.chunk.css"
+  },
+  {
+    "revision": "3595b8abae663447055e",
+    "url": "/static/js/main.3595b8ab.chunk.js"
+  },
+  {
+    "revision": "5bd865d7ddebb5e98161",
+    "url": "/static/css/1.c8bf2d4d.chunk.css"
+  },
+  {
+    "revision": "5bd865d7ddebb5e98161",
+    "url": "/static/js/1.5bd865d7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c62a3087a0c78aa50db9888e3384c8c",
+    "url": "/static/media/me.3c62a308.jpg"
+  },
+  {
+    "revision": "ff3da4636a1b266764d097f9a67429b6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.f1f7e5764a5517f323ad0afc26419e30.js"
+  "/precache-manifest.c58cce0fdfebdf009d301393d5e86281.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/me/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
